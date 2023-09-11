dd083b5fcf9d5f61a555a7bb547ae69a2a2915bb
IP <ilya.pavlovski@gmail.com>
2023-09-11 16:11:37 -0300

    Birds plogpost summary.

--- src/app.tsx
+++ src/app.tsx
@@ -2,14 +2,7 @@ import { PiClockBold, PiTag } from 'react-icons/pi'
 import { css } from 'styled-system/css'
 import { Flex } from 'styled-system/jsx'
 
-type HeaderProps = {
-  image: string
-  tags: string[]
-  title: string
-  subtitle: string
-  date: string
-}
-
+type HeaderProps = { image: string; tags: string[]; title: string; subtitle: string; date: string }
 function Header(props: HeaderProps) {
   const { image, tags, title, subtitle, date } = props
   const styles = css({
@@ -18,7 +11,6 @@ function Header(props: HeaderProps) {
     color: '#fff',
     padding: '3rem',
     borderRadius: '1rem',
-    fontFamily: 'jakarta',
     display: 'flex',
     flexDir: 'column',
     minHeight: '400px',
@@ -61,8 +53,8 @@ function Header(props: HeaderProps) {
         </Flex>
       </Flex>
       <div>
-        <Flex gap='.25rem' align='center' opacity='.8'>
-          <PiTag style={{marginRight: '1rem'}}/>
+        <Flex gap=".25rem" align="center" opacity=".8">
+          <PiTag style={{ marginRight: '1rem' }} />
           {tags.map((tag) => (
             <a key={tag}>{tag}</a>
           ))}
@@ -74,12 +66,56 @@ function Header(props: HeaderProps) {
   )
 }
 
-export default function App() {
+type SummaryProps = { readingTime: number; views: number; date: string }
+function Summary(props: SummaryProps) {
+  const { readingTime, date, views } = props
+
   const styles = css({
-    margin: '4rem 6rem'
+    borderRadius: '1rem',
+    border: '2px solid #ededed',
+    justifyContent: 'space-between',
+    display: 'flex',
+    marginY: '3rem',
+    padding: '2rem',
+    '& h2': {
+      fontSize: '1rem',
+      fontWeight: 'bold',
+      opacity: 0.4
+    },
+    '& h1': {
+      fontSize: '1.5rem',
+      fontWeight: 'bold',
+      opacity: 0.8
+    }
   })
 
-  const headerArgs: HeaderProps = {
+  return (
+    <div className={styles}>
+      <section>
+        <h2>Reading Time</h2>
+        <h1>{readingTime} mins</h1>
+      </section>
+      <section>
+        <h2>Views</h2>
+        <h1>{views} views</h1>
+      </section>
+      <section>
+        <h2>Published Date</h2>
+        <h1>{date}</h1>
+      </section>
+    </div>
+  )
+}
+
+
+
+export default function App() {
+  const styles = css({
+    margin: '4rem 6rem',
+    fontFamily: 'jakarta'
+  })
+
+  const headerArgs = {
     image:
       'https://images.unsplash.com/photo-1520808663317-647b476a81b9?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2073&q=80',
     tags: ['Nature', 'Animal', 'Stuff'],
@@ -87,11 +123,18 @@ export default function App() {
     subtitle: `Lorem ipsum dolor sit amet consectetur adipisicing elit.
     Lorem ipsum dolor sit amet.`,
     date: 'a year ago'
-  }
+  } satisfies HeaderProps
+
+  const summaryArgs = {
+    date: 'April 25, 2022',
+    readingTime: 6,
+    views: 1288
+  } satisfies SummaryProps
 
   return (
     <div className={styles}>
       <Header {...headerArgs} />
+      <Summary {...summaryArgs} />
     </div>
   )
 }
