7a25ea70329c48cb1d81b5e4e34ba9f71bfa326a
IP <ilya.pavlovski@gmail.com>
2023-09-12 17:27:44 -0300

    Basic nextjs async.

--- app/layout.tsx
+++ app/layout.tsx
@@ -8,10 +8,26 @@ export const metadata = {
   description: 'Not actually generated  by next-create-app'
 }
 
-export default function RootLayout({ children }: { children: React.ReactNode }) {
+type Time = {
+  datetime: string
+}
+
+export default async function RootLayout({ children }: { children: React.ReactNode }) {
+  const res = await fetch('http://worldtimeapi.org/api/timezone/America/Chicago', {
+    // on every request bypass caching, get new data
+    // cache: 'no-store'
+    next: {
+      revalidate: 5
+    }
+  })
+  const data: Time = await res.json()
+
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <h1>data: {data.datetime}</h1>
+        {children}
+      </body>
     </html>
   )
 }
