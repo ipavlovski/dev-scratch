8039dcb6613b484c30750a71721de47df2e04b1e
IP <ilya.pavlovski@gmail.com>
2023-09-12 21:12:28 -0300

    Ecomm scaffold page.

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,8 @@
 import { Inter } from 'next/font/google'
+import { FaCanadianMapleLeaf } from 'react-icons/fa'
+import { TbBrandNextjs, TbShoppingCart } from 'react-icons/tb'
+import { css, cx } from 'styled-system/css'
+import { HStack } from 'styled-system/jsx'
 import './globals.css'
 
 const inter = Inter({ subsets: ['latin'] })
@@ -8,25 +12,107 @@ export const metadata = {
   description: 'Not actually generated  by next-create-app'
 }
 
-type Time = {
-  datetime: string
+function Navbar() {
+  const linkStyles = css({
+    display: 'flex',
+    gap: '1rem',
+    margin: '.75rem',
+    flex: '1',
+    '& li': {
+      fontSize: '.825rem'
+    }
+  })
+
+  const logoStyles = css({
+    marginLeft: '.5rem'
+  })
+  const titleStyles = css({
+    textTransform: 'uppercase',
+    fontWeight: 'bold'
+  })
+
+  const shoppingCartStyles = css({
+    border: '2px solid',
+    borderColor: 'slate.200',
+    padding: '.5rem',
+    borderRadius: '.75rem',
+    margin: '.5rem 1rem'
+  })
+
+  return (
+    <HStack height="4rem">
+      <div>
+        <HStack mr={'2rem'}>
+          <TbBrandNextjs size={'2rem'} className={logoStyles} />
+          <h1 className={titleStyles}>Acme Store</h1>
+        </HStack>
+      </div>
+      <ul className={linkStyles}>
+        <li>All</li>
+        <li>Shirts</li>
+        <li>Stickers</li>
+      </ul>
+
+      <div className={shoppingCartStyles}>
+        <TbShoppingCart />
+      </div>
+    </HStack>
+  )
+}
+
+function Footer() {
+  const footerStyles = css({
+    background: 'slate.900',
+    paddingTop: '1rem'
+  })
+
+  const upperFooterStyles = css({
+    margin: '2rem',
+    '& li': {
+      paddingY: '.25rem'
+    }
+  })
+
+  const lowerFooterStyles = css({
+    padding: '.5rem 1rem',
+    borderTop: '2px solid',
+    fontSize: '.75rem',
+    display: 'flex',
+    gap: '.25rem',
+    alignItems: 'center'
+  })
+
+  return (
+    <div className={footerStyles}>
+      <div className={upperFooterStyles}>
+        <ul>
+          <li>Home</li>
+          <li>About</li>
+          <li>Terms & Conditions</li>
+          <li>Shipping & Return Policy</li>
+          <li>Privacy Policy</li>
+          <li>FAQ</li>
+        </ul>
+      </div>
+      <div className={lowerFooterStyles}>
+        <p>© 2023 OceanSplash. All rights reserved | Designed and developed in Nova Scotia</p>
+        <FaCanadianMapleLeaf size={'.875rem'} />
+      </div>
+    </div>
+  )
 }
 
 export default async function RootLayout({ children }: { children: React.ReactNode }) {
-  const res = await fetch('http://worldtimeapi.org/api/timezone/America/Chicago', {
-    // on every request bypass caching, get new data
-    // cache: 'no-store'
-    next: {
-      revalidate: 5
-    }
+  const styles = css({
+    background: 'slate.800',
+    color: 'slate.200'
   })
-  const data: Time = await res.json()
-
   return (
     <html lang="en">
-      <body className={inter.className}>
-        <h1>data: {data.datetime}</h1>
+      <body className={cx(inter.className, styles)}>
+        <Navbar />
         {children}
+        <Footer />
       </body>
     </html>
   )
--- app/page.tsx
+++ app/page.tsx
@@ -1,32 +1,15 @@
 import Link from 'next/link'
 import { css } from 'styled-system/css'
-import { Stack } from 'styled-system/jsx'
+import { Center, Stack } from 'styled-system/jsx'
 
 export default function Home() {
   const styles = css({
-    fontSize: '3xl',
-    fontWeight: 'bold',
-    textTransform: 'uppercase',
-    _hover: {
-      background: 'emerald.500'
-    }
+    fontSize: '3xl'
   })
 
   return (
-    <Stack height="100vh" background="slate.500" color="slate.100">
-      <div className={styles}>Hello panda 🐼!</div>
-      <h1>Some links</h1>
-      <ul>
-        <li>
-          <Link href="/blog/123">BLOG 123</Link>
-        </li>
-        <li>
-          <Link href="/about">ABOUT</Link>
-        </li>
-        <li>
-          <Link href="/fetching">FETCHING</Link>
-        </li>
-      </ul>
-    </Stack>
+    <Center height="calc(100vh - 4rem)">
+      <div>This is the home page</div>
+    </Center>
   )
 }
