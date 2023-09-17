146404750336c4349ff575a8384e4233b74979f1
IP <ilya.pavlovski@gmail.com>
2023-09-17 09:33:59 -0300

    Simplest headless-ui transition.

--- src/app2.tsx
+++ src/app2.tsx
@@ -1,106 +1,84 @@
-import { a, useSpring } from '@react-spring/web'
+import { Dialog } from '@headlessui/react'
+import { a, useSpring, useTransition } from '@react-spring/web'
 import { useState } from 'react'
 import { css } from 'styled-system/css'
 import { styled } from '../styled-system/jsx'
 
-// function Stuff() {
-//   return <div style={{backgr}}></div>
-// }
+type ToggleState = [boolean, React.Dispatch<React.SetStateAction<boolean>>]
 
-function AnimatedSmth() {
-  // const springs = useSpring({
-  //   from: { x: 0 },
-  //   to: { x: 500 },
-  //   // loop: true
-  // })
+function MyDialog({ toggle }: { toggle: ToggleState }) {
+  const [isShowing, setShowing] = toggle
 
-  const [springs, api] = useSpring(() => ({
-    from: { x: 150 }
-    // x: 300
-    // from: { x: 300 },
-    // backgroundColor: '#ff',
-    // scale: [1, 1, 1],
-    // config: {
-    //   duration: 150
-    // }
-  }))
-
-  const handleClick = () => {
-    api.start({
-      from: {
-        x: 0
-      },
-      to: {
-        x: 500
-      }
-    })
+  const styles = {
+    dialog: css({
+      position: 'absolute',
+      width: '200px',
+      height: '200px',
+      left: '100px',
+      top: '100px',
+      background: 'gray.300'
+    }),
+    backdrop: css({}),
+    container: css({}),
+    panel: css({}),
+    title: css({})
   }
 
   return (
-    <a.div
-      onClick={handleClick}
-      style={{
-        width: 80,
-        height: 80,
-        background: 'hsl(0, 100%, 71%)',
-        borderRadius: 8,
-        ...springs
-      }}></a.div>
+    <Dialog open={isShowing} onClose={() => setShowing(false)} className={styles.dialog}>
+      {/* The backdrop, rendered as a fixed sibling to the panel container */}
+      <div className={styles.backdrop} aria-hidden="true" />
+
+      {/* Full-screen container to center the panel */}
+      <div className={styles.container}>
+        {/* The actual dialog panel  */}
+        <Dialog.Panel className={styles.panel}>
+          <Dialog.Title>Complete your order</Dialog.Title>
+
+          <p>This is an item</p>
+        </Dialog.Panel>
+      </div>
+    </Dialog>
   )
 }
 
+function Button({ toggle }: { toggle: ToggleState }) {
+  const [isShowing, setShowing] = toggle
 
-function Drawer({ show }: { show: boolean }) {
-  const props = useSpring({
-    left: show ? window.innerWidth - 300 : window.innerWidth,
-    top: 0,
-    backgroundColor: '#806290',
-    height: '100vh',
-    width: '300px'
-  })
-
-  return (
-    <a.div style={{ position: 'absolute', ...props }}>
-      <div className="drawer">Animated Drawer!</div>
-    </a.div>
-  )
-}
-
-type ToggleState = [boolean, React.Dispatch<React.SetStateAction<boolean>>]
-
-function Button({ props: [isShowing, setter] }: { props: ToggleState }) {
   const styles = css({
-    backgroundColor: '#719c70',
-    color: 'white',
-    padding: '10px 15px',
-    fontSize: 20,
-    borderRadius: 8,
-    border: 'none',
-    cursor: 'pointer'
+    color: 'slate.100',
+    cursor: 'pointer',
+    backgroundColor: 'yellow.800',
+    padding: '.5em',
+    rounded: 'xl',
+    textTransform: 'uppercase',
+    _hover: { bg: 'yellow.700' }
   })
 
   return (
-    <button className={styles} onClick={() => setter(!isShowing)}>
-      {isShowing ? 'Close' : 'Open'}
+    <button onClick={() => setShowing(!isShowing)} className={styles}>
+      {isShowing ? 'hide' : 'show'}
     </button>
   )
 }
 
-// SIMPLEST DRAWER EXAMPLE FROM ADRIANO TRIANA
-// https://medium.com/geekculture/building-an-animated-slide-in-drawer-with-react-spring-22a6a54bc4cd
 export default function App() {
   const styles = css({
-    background: 'slate.800',
+    bg: 'slate.800',
+    color: 'slate.100',
+    height: '100vh',
     width: '100vw',
-    height: '100vh'
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center'
   })
 
-  const [isShowing, setShowing] = useState(false)
+  const toggle = useState(false)
 
   return (
     <div className={styles}>
-      <Button props={[isShowing, setShowing]} />
-      <Drawer show={isShowing} />
+      <Button toggle={toggle} />
+      <MyDialog toggle={toggle} />
     </div>
   )
 }
